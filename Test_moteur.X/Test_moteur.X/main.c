72237af28f976b6a1125c0b19aace07b2e84fb1b
Mirague94 <43612203+Mirague94@users.noreply.github.com>
2020-01-06 15:44:39 +0100

    Mise sous etat des fonctions

--- Test_moteur.X/Test_moteur.X/main.c
+++ Test_moteur.X/Test_moteur.X/main.c
@@ -18,8 +18,8 @@ void main(void) {
     lcd_init(); //initialiser le lcd
     int finDeCourse=1, debutDeCourse;
     double moteurG, moteurD;
-    int vitesseMax = 180;
-    int timer;
+    double vitesseMax;
+    int etat=1;
     
     TRISEbits.TRISE1 = 1; // Jack sur RE1
     TRISEbits.TRISE2 = 1; // fin de course sur RE2
@@ -35,72 +35,47 @@ void main(void) {
         debutDeCourse = PORTEbits.RE1; //lecture sur RE2, pin 11 (Jack)
         finDeCourse = PORTEbits.RE2; //lecture Port E pin 14 (FDC)
         
-        if (debutDeCourse == 1)
-        {
-            pwm_init (150,2); // initialiser 2 moteurs pour une fréquence de 2kHz
-        }
+        vitesseMax = adc_read(0)/1023.0*600.0;
         
-        if ((n1<200)&& (n4>200))
+        switch (etat)
         {
-            moteurG = 0;
-            moteurD = 0;
-            lcd_position(1,0);
-            lcd_printf("MG %4d",(int) moteurG); // moteur gauche
-            lcd_position(1,8);
-            lcd_printf("MD %4d",(int) moteurD); // moteur droit
-            
-            break;
+            case 1:
+                if (debutDeCourse == 1)
+                {
+                pwm_init (150,2); // initialiser 2 moteurs pour une fréquence de 2kHz
+                etat = 2;
+                }
+                break;
+            case 2:
+                moteurG = vitesseMax * (1023-(double)n3)/1024;
+                moteurD = vitesseMax * (1023-(double)n2)/1024;
+                if (finDeCourse == 0) etat = 3;
+                break;
+            case 3:
+                moteurG = 0;
+                moteurD = 0;
+                break;
+            case 4:
+                CloseTimer0();
+                pwm_setdc1(0);
+                pwm_setdc2(180);
+                if ((n2>400) &&  (n3>400)) etat = 2;
+                break;
+                
+        }
 
-            /*OpenTimer0( TIMER_INT_ON &
-                        T0_16BIT &
-                        T0_SOURCE_INT &
-                        T0_PS_1_256);*/
-        }
-        
-        
-        /*while ((ReadTimer0()>3750) || ((n2>400) &&  (n3>400))) ;
-        {
-            CloseTimer0();
-            timer=ReadTimer0();
-            n2=adc_read(3); //AN3 pin 6
-            n3=adc_read(4); //AN4 pin 10
-            pwm_setdc1(0);
-            pwm_setdc2(180);
-            
-            lcd_position(0,0);
-            lcd_printf("%3d %7d %3d",n1,timer,n4); // Capteur
-            lcd_position(1,0);
-            lcd_printf("MG %4d",(int) moteurG); // moteur gauche
-            lcd_position(1,8);
-            lcd_printf("MD %4d",(int) moteurD); // moteur droit
-        }*/
-        
-        moteurG = vitesseMax * (1023-(double)n3)/1024;
-        moteurD = vitesseMax * (1023-(double)n2)/1024;
-        
-        pwm_setdc1(moteurG);
-        pwm_setdc2(moteurD);
+        pwm_setdc1((int)moteurG);
+        pwm_setdc2((int)moteurD);
         
         // affiche les données des capteurs
         lcd_position(0,0);
-        lcd_printf("%3d %7d %3d",n1,timer,n4); // Capteur
+        lcd_printf("%3d %3d %3d %3d",n1,n2,n3,n4); // Capteurs
         lcd_position(1,0);
-        lcd_printf("MG %4d",(int) moteurG); // moteur gauche
-        lcd_position(1,8);
-        lcd_printf("MD %4d",(int) moteurD); // moteur droit
-        
-        if (finDeCourse == 0)
-        {
-            moteurG = 0;
-            moteurD = 0;
-            lcd_position(1,0);
-            lcd_printf("MG %4d",(int) moteurG); // moteur gauche
-            lcd_position(1,8);
-            lcd_printf("MD %4d",(int) moteurD); // moteur droit
-            
-            break;
-        } 
+        lcd_printf("%4d",(int) moteurG); // moteur gauche
+        lcd_position(1,5);
+        lcd_printf("%4d",(int) vitesseMax); // moteur droit
+        lcd_position(1,10);
+        lcd_printf("%4d",(int) moteurD); // moteur droit
     }
     return;   
 }
-
